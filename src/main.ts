cdf87a56f4786c21f4891471d419815c6b8aea90
Lucas Candido <Lucas.Pereira-Silva@tecban.com.br>
2024-05-13 22:23:33 -0300

    adjusts initialization file so that I start the project as NestExpressApplication and start the microservices separately

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
+import { Config } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: ['amqp://guest:guest@localhost:5672'],
-        queue: 'order-service-queue',
-        queueOptions: {
-          durable: false,
-        },
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const configService = app.get(ConfigService);
+  const config = (configService as unknown as { internalConfig: Config })
+    .internalConfig;
+
+  await app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://guest:guest@localhost:5672'],
+      queue: 'order-service-queue',
+      queueOptions: {
+        durable: false,
       },
     },
-  );
+  });
 
-  await app.listen();
+  await app.startAllMicroservices();
+  await app.listen(+config.api.port);
 }
 bootstrap();
